d5b16bc31ee22e6a6a13feb5c14d532d05a0a112
Viktor Krejcir <viktor.krejcir@fibiworks.com>
2022-12-07 16:45:05 +0100

    fixing balanced

--- src/katas/numberkatas.clj
+++ src/katas/numberkatas.clj
@@ -6,13 +6,13 @@
 (defn handleEven
   "Handles sequence with even amount of items"
   [sequence totalDigits]
-  (if (==(reduce + (doall (take (-(quot totalDigits 2)1) sequence))) (reduce + (doall (take (-(quot totalDigits 2)1) (reverse sequence))))) "Balanced" "Not Balanced")
+  (if (==(reduce + (take (-(quot totalDigits 2)1) sequence)) (reduce + (take (-(quot totalDigits 2)1) (reverse sequence)))) "Balanced" "Not Balanced")
   )
 
 (defn handleOdd
   "Handles sequence with odd amount of items"
   [sequence totalDigits]
-  (if (==(reduce + (doall (take (quot totalDigits 2) sequence))) (reduce + (doall (take (quot totalDigits 2) (reverse sequence))))) "Balanced" "Not Balanced")
+  (if (==(reduce + (take (quot totalDigits 2) sequence)) (reduce + (take (quot totalDigits 2) (reverse sequence)))) "Balanced" "Not Balanced")
   )
 
   (defn balanced-num
@@ -22,9 +22,6 @@
     (let [converted-digits (digits num)
           total-digits (count converted-digits)]
       (cond
-        (== total-digits 0) "Balanced"
-        (== total-digits 1) "Balanced"
-        (== total-digits 2) "Balanced"
         (even? total-digits) (handleEven converted-digits total-digits)
         :else (handleOdd converted-digits total-digits)
         ))
