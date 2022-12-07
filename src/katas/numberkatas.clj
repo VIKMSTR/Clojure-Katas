f3e37e9e11f33973963870fb5bbfad0ae7b56baa
Viktor Krejcir <viktor.krejcir@fibiworks.com>
2022-12-07 16:59:10 +0100

    renaming balanced

--- src/katas/numberkatas.clj
+++ src/katas/numberkatas.clj
@@ -3,13 +3,13 @@
 (defn digits [n]
   (->> n str (map (comp read-string str))))
 
-(defn handleEven
+(defn handleEvenBalanced
   "Handles sequence with even amount of items"
   [sequence totalDigits]
   (if (==(reduce + (take (-(quot totalDigits 2)1) sequence)) (reduce + (take (-(quot totalDigits 2)1) (reverse sequence)))) "Balanced" "Not Balanced")
   )
 
-(defn handleOdd
+(defn handleOddBalanced
   "Handles sequence with odd amount of items"
   [sequence totalDigits]
   (if (==(reduce + (take (quot totalDigits 2) sequence)) (reduce + (take (quot totalDigits 2) (reverse sequence)))) "Balanced" "Not Balanced")
@@ -22,7 +22,7 @@
     (let [converted-digits (digits num)
           total-digits (count converted-digits)]
       (cond
-        (even? total-digits) (handleEven converted-digits total-digits)
-        :else (handleOdd converted-digits total-digits)
+        (even? total-digits) (handleEvenBalanced converted-digits total-digits)
+        :else (handleOddBalanced converted-digits total-digits)
         ))
     )
